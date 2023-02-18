1c027d5b3b2fab7df3bc6e375b5e2f2e75ab315c
Matthew Harvey <matthewtlharvey@gmail.com>
2023-02-18 16:12:27 +0000

    update nav on /login

--- pages/login.tsx
+++ pages/login.tsx
@@ -35,24 +35,18 @@ export default function Login({loggedin}:any) {
     const supabase = useSupabaseClient();
     const session = useSession();
     const router = useRouter();
+    if (session != undefined && loggedin == false) {
+        router.push({
+            pathname: '/login',
+            query: {},
+        })
+    }
     return (
         <>
             <Nav isloggedin={loggedin} />
             <div className='grid p-2 sm:grid-cols-1 md:grid-cols-1 mt-28 m-auto text-center'>
                 <div className='max-w-xl m-auto text-center text-lg'>
-                    {session &&
-                        <>
-                            <div>
-                                <p className='mt-40 mb-2'><u><b>{session.user?.email}</b></u></p>
-                                <p className='mb-4'>You are now Logged In.</p>
-                                <button onClick={()=> router.back()}
-                                    className="inline-block rounded-lg bg-slate-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-md hover:bg-slate-500 hover:text-white hover:scale-110 ease-in-out transition">
-                                    Return to where you left off.
-                                </button>
-                            </div>
-                        </>
-                    }
-                    {!session &&
+                    {!session ? (
                         <>
                             <h1 className='font-semibold text-2xl p-2'>To create/view lists you must login:</h1>
                             <p>Demo credentials:
@@ -76,7 +70,18 @@ export default function Login({loggedin}:any) {
                                 }}
                             />
                         </>
-                    }
+                    ) : (
+                        <>
+                            <div>
+                                <p className='mt-40 mb-2'><u><b>{session.user?.email}</b></u></p>
+                                <p className='mb-4'>You are now Logged In.</p>
+                                <button onClick={()=> router.back()}
+                                    className="inline-block rounded-lg bg-slate-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-md hover:bg-slate-500 hover:text-white hover:scale-110 ease-in-out transition">
+                                    Return to where you left off.
+                                </button>
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
         </>
