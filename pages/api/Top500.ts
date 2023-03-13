d097db5b21813999c6aa796b5d747aac70b5dfe8
Matthew Harvey <matthewtlharvey@gmail.com>
2023-03-13 02:04:54 +0000

    trivia feature basics

---
+++ pages/api/Top500.ts
@@ -0,0 +1,54 @@
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const baseimg = "https://image.tmdb.org/t/p/w500";
+
+export default async function Top500(req: NextApiRequest, res: NextApiResponse) {
+    const supabase = createBrowserSupabaseClient();
+    const checkmonth = await supabase.from("top500").select().eq("month", new Date().getMonth().toString() + new Date().getFullYear().toString());
+    // @ts-ignore
+    let new_month = false;
+    try {
+        let count = 0;
+        for (var x in checkmonth.data){
+            count++;
+        }
+        if (count == 0) {
+            new_month = true;
+        }
+    } catch {}
+    if (new_month == true) {
+        let counter = 1;
+        let movie_arr: any[] = [];
+        let tv_arr: any[] = [];
+        while ( counter < 20) {
+            const moviepage = await fetch("https://api.themoviedb.org/3/movie/top_rated?page=" + counter + "&api_key=" + process.env.NEXT_PUBLIC_APIKEY?.toString() + "&language=en-US&include_adult=false").then((response) => response.json());
+            moviepage.results.forEach((movie: { title: string; popularity: number; poster_path: string; id: number, original_language: string, genre_ids: any[], revenue: string, release_date: string, vote_average: number}) => {
+                var imgurl = "";
+                if (movie.poster_path == null){
+                    imgurl = "https://eu.ui-avatars.com/api/?name=" + movie.title;
+                } else {
+                    imgurl = baseimg + movie.poster_path;
+                }
+                if (movie.original_language == 'en') {
+                    movie_arr.push([movie.title, movie.popularity, imgurl, movie.id, movie.genre_ids, movie.release_date, movie.vote_average])
+                }
+            });
+            const tvpage = await fetch("https://api.themoviedb.org/3/tv/top_rated?page=" + counter + "&api_key=" + process.env.NEXT_PUBLIC_APIKEY?.toString() + "&language=en-US&include_adult=false").then((response) => response.json());
+            tvpage.results.forEach((tv: { name: string; popularity: number; poster_path: string; id: number, original_language: string, genre_ids: any[], first_air_date: string, vote_average: number}) => {
+                var imgurl = "";
+                if (tv.poster_path == null){
+                    imgurl = "https://eu.ui-avatars.com/api/?name=" + tv.name;
+                } else {
+                    imgurl = baseimg + tv.poster_path;
+                }
+                if (tv.original_language == 'en') {
+                    tv_arr.push([tv.name, tv.popularity, imgurl, tv.id, tv.genre_ids, tv.first_air_date, tv.vote_average])
+                }
+            });
+            counter++;
+        }
+        await supabase.from("top500").upsert({"month": new Date().getMonth().toString() + new Date().getFullYear().toString(), "movie": movie_arr, "tv": tv_arr})
+    }
+    res.status(200).json({result: "Updated top rated tv/movie. "});
+}
--- pages/trivia.tsx
+++ pages/trivia.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/rules-of-hooks */
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
@@ -5,10 +6,11 @@ import { Auth, ThemeSupa } from '@supabase/auth-ui-react';
 import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { GetServerSidePropsContext, PreviewData, NextApiRequest, NextApiResponse } from 'next';
 import { ParsedUrlQuery } from 'querystring';
-import axios from 'axios';
 import { useState } from 'react';
 import Nav from '../components/Nav';
 import { getAvatarName } from '../functions/getAvatarName';
+import router from 'next/router';
+import axios from 'axios';
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext<ParsedUrlQuery, PreviewData> | { req: NextApiRequest; res: NextApiResponse<any>; }) => {
     // Create authenticated Supabase Client
@@ -17,6 +19,16 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext<ParsedUr
     const {
         data: { session },
     } = await supabase.auth.getSession()
+
+    const updateTop500 = await axios.get(process.env.NEXT_PUBLIC_BASEURL?.toString() + "api/Top500");
+
+    const currentTop500 = await supabase.from("top500").select().eq("month", new Date().getMonth().toString() + new Date().getFullYear().toString());
+    // @ts-ignore
+    let movie = currentTop500.data[0].movie; let tv = currentTop500.data[0].tv;
+
+    let movie_count = 0; let tv_count = 0;
+    for (var x in movie) {movie_count++;};for (var y in tv) {tv_count++;};
+    movie_count = movie_count-1; tv_count = tv_count-1;
 
     let UserData = await getAvatarName(session);
     // @ts-ignore
@@ -33,79 +45,168 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext<ParsedUr
         }
     }
 
-    const { data } = await supabase
-    .from('quizcontent')
-    .select('quizid, quizcontent')
-    .eq('userid', session?.user.id)
-
     return {
         props: {
-            userquiz: data,
             loggedin: true,
             username,
-            avatar
+            avatar,
+            movie,
+            tv,
+            movie_count,
+            tv_count
         },
     }
 }
 
-async function CreateQuiz(userid: string, router: any) { 
-    const getResult = await axios.get(process.env.NEXT_PUBLIC_BASEURL?.toString() + "api/CreateQuiz", {params: {userid: userid}});
-    router.push({
-        pathname: '/quiz/[quizid]',
-        query: { quizid: getResult.data.quizid },
-    })
-}
-
-export default function Quiz(this: any, {userquiz, loggedin, username, avatar}: any) {
+export default function Trivia(this: any, {loggedin, username, avatar, movie, tv, movie_count, tv_count}: any) {
     const supabase = useSupabaseClient();
     const session = useSession();
 
-    const [amountnum, setAmountNum] = useState("5");
-    const [order, setOrder] = useState("");
-    const [type_ofshow, setTypeOfShow] = useState("");
+    const [order, setOrder] = useState("rel");
+    const [type_ofshow, setTypeOfShow] = useState("mov");
+    const [border, setBorder] = useState(0);
+    const [clicked, setClicked] = useState(false);
 
-    function GenerateRandom(): void {
-        console.log(amountnum, order, type_ofshow);
+    const [m1, setM1] = useState(randomIntFromInterval(0, movie_count/2));
+    const [m2, setM2] = useState(randomIntFromInterval(((movie_count/2) + 1), movie_count));
+    const [t1, setT1] = useState(randomIntFromInterval(0, tv_count/2));
+    const [t2, setT2] = useState(randomIntFromInterval(((tv_count/2) + 1), tv_count));
+    const triviaArr_range_mov = [movie[m1], movie[m2]];
+    const triviaArr_range_tv = [tv[t1], tv[t2]];
+
+    function randomIntFromInterval(min: number, max: number) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min)
     }
+    function newOptions () {
+        let a; let b;
+        if (type_ofshow == "mov") {
+            a = randomIntFromInterval(0, movie_count);
+            b = randomIntFromInterval(0, movie_count);
+            while (a == b) {a = randomIntFromInterval(0, movie_count);b = randomIntFromInterval(0, movie_count);}
+            setM1(a);
+            setM2(b);
+        } else {
+            a = randomIntFromInterval(0, tv_count);
+            b = randomIntFromInterval(0, tv_count);
+            while (a == b) {a = randomIntFromInterval(0, tv_count);b = randomIntFromInterval(0, tv_count);}
+            setT1(a);
+            setT2(b);
+        }
+    }
+
+    function GetAnswer (arr: any[], movieid: number) {
+        let other_index = 0;
+        let index = 1;
+        let other_movieid = arr[0][3];
+        if (arr[0][3] == movieid){other_index=1;index=0;other_movieid = arr[1][3];};
+        // order = rel, rev, sco, len
+        let bool;
+        if (order == "rel") {
+            bool = new Date(arr[index][5]) < new Date(arr[other_index][5]);
+        } if (order == "sco") {
+            bool = arr[index][6] < arr[other_index][6];
+        }
+
+        if (bool) {
+            setBorder(movieid);
+        } else {
+            setBorder(other_movieid);
+        }
+        setClicked(true);
+    }
+    
+    // @ts-ignore
+    const display_movies = triviaArr_range_mov.map((m) =>
+        <>
+            {clicked && border == m[3] ?
+                <>
+                    <div key={m[3]} className="group cursor-pointer relative inline-block text-center ring-4 ring-green-400 rounded-xl">
+                        <button onClick={() => GetAnswer(triviaArr_range_mov, m[3])}>
+                            <img id={m[3].toString()} src={m[2].toString()} alt={m[0].toString()} className="rounded-3xl w-80 p-2 h-full" />
+                            <div className="absolute bottom-0 flex-col items-center hidden mb-6 group-hover:flex">
+                                <span className="z-10 p-3 text-md leading-none rounded-lg text-white whitespace-no-wrap bg-gradient-to-r from-blue-700 to-red-700 shadow-lg">
+                                    {m[0]}
+                                </span>
+                            </div>
+                        </button>
+                    </div>
+                </>
+                :
+                <>
+                    <div key={m[3]} className="group cursor-pointer relative inline-block text-center">
+                        <button onClick={() => GetAnswer(triviaArr_range_mov, m[3])}>
+                            <img id={m[3].toString()} src={m[2].toString()} alt={m[0].toString()} className="rounded-3xl w-80 p-2 h-full" />
+                            <div className="absolute bottom-0 flex-col items-center hidden mb-6 group-hover:flex">
+                                <span className="z-10 p-3 text-md leading-none rounded-lg text-white whitespace-no-wrap bg-gradient-to-r from-blue-700 to-red-700 shadow-lg">
+                                    {m[0]}
+                                </span>
+                            </div>
+                        </button>
+                    </div>
+                </>
+            }
+        </>
+    );
+    // @ts-ignore
+    const display_tv = triviaArr_range_tv.map((t) =>
+        <>
+            {clicked && border == t[3] ?
+                <>
+                    <div key={t[3]} className="group cursor-pointer relative inline-block text-center ring-4 ring-green-400 rounded-xl">
+                        <button onClick={() => GetAnswer(triviaArr_range_tv, t[3])}>
+                            <img id={t[3].toString()} src={t[2].toString()} alt={t[0].toString()} className="rounded-3xl w-80 p-2 h-full" />
+                            <div className="absolute bottom-0 flex-col items-center hidden mb-6 group-hover:flex">
+                                <span className="z-10 p-3 text-md leading-none rounded-lg text-white whitespace-no-wrap bg-gradient-to-r from-blue-700 to-red-700 shadow-lg">
+                                    {t[0]}
+                                </span>
+                            </div>
+                        </button>
+                    </div>
+                </>
+                :
+                <>
+                    <div key={t[3]} className="group cursor-pointer relative inline-block text-center">
+                        <button onClick={() => GetAnswer(triviaArr_range_tv, t[3])}>
+                            <img id={t[3].toString()} src={t[2].toString()} alt={t[0].toString()} className="rounded-3xl w-80 p-2 h-full" />
+                            <div className="absolute bottom-0 flex-col items-center hidden mb-6 group-hover:flex">
+                                <span className="z-10 p-3 text-md leading-none rounded-lg text-white whitespace-no-wrap bg-gradient-to-r from-blue-700 to-red-700 shadow-lg">
+                                    {t[0]}
+                                </span>
+                            </div>
+                        </button>
+                    </div>
+                </>
+            }
+        </>
+    );
+
+    let text = "Please select the ";
+    if (type_ofshow == "mov"){text = text + "movie that "}else{text = text + "tv show that "};
+    if (order == "rel"){text = text + "released first."}; if (order == "rev"){text = text + "generated the most revenue."}; 
+    if (order == "sco"){text = text + "has the higher imdb rating."};if (order == "len"){text = text + "has the longest runtime."};
 
     return (
         <>
             <Nav isloggedin={loggedin} username={username} avatar={avatar} />
             <div className='mt-6 m-auto'>
-                <div className='grid grid-cols-1 md:grid-cols-3 max-w-6xl m-auto'>
+                <div className='grid grid-cols-1 md:grid-cols-2 max-w-6xl m-auto'>
                     <div className='p-4'>
-                        <label htmlFor="type" className="block mb-2 text-m font-medium text-gray-900 dark:text-white m-auto">Type</label>
-                        <select id="type" value={type_ofshow} onChange={(e) => setTypeOfShow(e.target.value)} className="m-auto bg-gray-50 border border-gray-300 text-gray-900 text-m rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <label htmlFor="type" className="block mb-2 text-m font-medium text-slate-600 dark:text-white m-auto">Type</label>
+                        <select id="type" value={type_ofshow} onChange={(e) => setTypeOfShow(e.target.value)}className="m-auto bg-gray-50 border border-gray-300 text-gray-900 text-m rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             <option value="mov">Movie</option>
                             <option value="tv">Tv Shows</option>
                         </select>
                     </div>
                     <div className='p-4'>
-                        <label htmlFor="order" className="block mb-2 text-m font-medium text-gray-900 dark:text-white m-auto">Order By</label>
+                        <label htmlFor="order" className="block mb-2 text-m font-medium text-slate-600 dark:text-white m-auto">Compare By</label>
                         <select id="order" value={order} onChange={(e) => setOrder(e.target.value)} className="m-auto bg-gray-50 border border-gray-300 text-gray-900 text-m rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                            <option value="release">Release Date</option>
-                            <option value="rev">Revenue</option>
-                            <option value="score">Imdb Score</option>
-                            <option value="FR">Length</option>
+                            <option value="rel">Release Date</option>
+                            <option value="sco">Imdb Score</option>
                         </select>
                     </div>
-                    <div className='p-4'>
-                        <label htmlFor="amountnum" className="block mb-2 text-m font-medium text-gray-900 dark:text-white m-auto">Amount</label>
-                        <input id="amountnum" value={amountnum} onChange={(e) => setAmountNum(e.target.value)} type="number" className="m-auto bg-gray-50 border border-gray-300 text-gray-900 text-m rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                    </div>
-                </div>
-                <div className="grid grid-cols-2 max-w-6xl m-auto py-4 gap-6">
-                    <button onClick={()=> GenerateRandom()} 
-                        className="inline-block rounded-lg bg-yellow-600 px-4 py-1.5 text-base font-semibold leading-7 text-black shadow-md hover:bg-orange-500 hover:text-white hover:scale-110 ease-in-out transition">
-                         Generate Random Trivia 
-                    </button>
-                    <button onClick={()=> GenerateRandom()} 
-                        className="inline-block rounded-lg bg-slate-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-md hover:bg-slate-500 hover:text-white hover:scale-110 ease-in-out transition">
-                         Create Custom Trivia 
-                    </button>
                 </div>
             </div>
-            <div className='grid p-2 sm:grid-cols-1 md:grid-cols-1 mt-28 m-auto'>
+            <div className='grid p-2 sm:grid-cols-1 md:grid-cols-1 m-auto'>
                 {!session ? (
                     <>
                         <div className='max-w-xl m-auto text-center text-lg'>
@@ -134,8 +235,19 @@ export default function Quiz(this: any, {userquiz, loggedin, username, avatar}: 
                     </>
                 ) : (
                     <>
-                        <div className='max-w-6xl p-10 justify-center m-auto'>
-                            <p className='mb-6 text-lg font-semibold'>Logged in using - {session.user.email}</p>
+                        <button onClick={() => newOptions()} className="max-w-xs m-auto inline-block rounded-lg bg-yellow-600 px-4 py-1.5 text-base font-semibold leading-7 text-black shadow-md hover:bg-orange-500 hover:text-white hover:scale-110 ease-in-out transition">Get new options</button>
+                        <p className='max-w-6xl p-4 m-auto'>{text}</p>
+                        <div className='max-w-6xl p-4 justify-center m-auto grid grid-cols-2 gap-6'>
+                            {type_ofshow == "mov" && 
+                                <>
+                                    {display_movies}
+                                </>
+                            }
+                            {type_ofshow == "tv" && 
+                                <>
+                                    {display_tv}
+                                </>
+                            }
                         </div>
                     </>
                 )}
